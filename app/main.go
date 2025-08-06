d711dcd6b57ca40e9f3c95c54eba9d83b9785dae
hegel.ye <Hegel.Ye@batechworks.com>
2025-08-06 15:11:14 +0800

    get number of tables

--- app/main.go
+++ app/main.go
@@ -22,6 +22,7 @@ func main() {
 			log.Fatal(err)
 		}
 
+		// first 100 bytes is sqlite db header
 		header := make([]byte, 100)
 
 		_, err = databaseFile.Read(header)
@@ -38,7 +39,21 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 		// Uncomment this to pass the first stage
-		fmt.Printf("database page size: %v", pageSize)
+		fmt.Printf("database page size: %v\n", pageSize)
+
+		pageHeader := make([]byte, 8)
+		// Read the first page header
+		_, err = databaseFile.Read(pageHeader)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var cellCount uint16
+		if err := binary.Read(bytes.NewReader(pageHeader[3:5]), binary.BigEndian, &cellCount); err != nil {
+			fmt.Println("Failed to read integer:", err)
+			return
+		}
+		fmt.Printf("number of tables: %v\n", cellCount)
 	default:
 		fmt.Println("Unknown command", command)
 		os.Exit(1)
